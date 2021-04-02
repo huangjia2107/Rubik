2d1a489ab7b406fb8e6557b1cec25341473a1ada
huangjia <654664168@qq.com>
2021-04-02 18:31:46 +0800

    add display viewmodel

---
+++ src/Rubik.Demo/ViewModels/DisplayDemoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Prism.Mvvm;
+using Rubik.Service.Models;
+
+namespace Rubik.Demo.ViewModels
+{
+    public class DisplayDemoViewModel : BindableBase
+    {
+        public DemoType Type { get; set; }
+        public List<DemoViewModel> DemoList { get; set; }
+
+        private bool _isChecked;
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set { SetProperty(ref _isChecked, value); }
+        }
+    }
+}
--- src/Rubik.Demo/ViewModels/HomeControlViewModel.cs
+++ src/Rubik.Demo/ViewModels/HomeControlViewModel.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using Prism.Mvvm;
+using Rubik.Demo.Models;
 
 namespace Rubik.Demo.ViewModels
 {
     public class HomeControlViewModel : BindableBase
     {
+        private AppData _appData = null;
+
+        public HomeControlViewModel(AppData appData)
+        {
+            _appData = appData;
+        }
+
+        public List<DisplayDemoViewModel> AllDemos
+        {
+            get
+            {
+                if (_appData.InternalData.DemoModels == null || _appData.InternalData.DemoModels.Length == 0)
+                    return null;
+
+                return _appData.InternalData.DemoModels.GroupBy(d => d.Type)
+                                                       .Select(g => new DisplayDemoViewModel 
+                                                       { 
+                                                           Type = g.Key, 
+                                                           DemoList = g.Select(d => new DemoViewModel(d)).ToList() 
+                                                       }).ToList();
+            }
+        }
     }
 }
