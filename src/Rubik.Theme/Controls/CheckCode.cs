1236fd55686084d974d4a0bda88583df140f7388
yanjinhuamail@gmail.com <yanjinhuamail@gmail.com>
2021-05-07 17:15:05 +0800

    【updater】验证码

---
+++ src/Rubik.Theme/Controls/CheckCode.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Rubik.Theme.Controls
+{
+    [TemplatePart(Name = ImageTemplateName, Type = typeof(System.Windows.Controls.Image))]
+    public class CheckCode : System.Windows.Controls.Control
+    {
+
+        private const string ImageTemplateName = "PART_Image";
+        private System.Windows.Controls.Image _image;
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(CheckCode),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Journal | FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// 随机生成的验证码
+        /// </summary>
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+
+        public int CodeWidth
+        {
+            get { return (int)GetValue(CodeWidthProperty); }
+            set { SetValue(CodeWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CodeWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CodeWidthProperty =
+            DependencyProperty.Register("CodeWidth", typeof(int), typeof(CheckCode), new PropertyMetadata(200));
+
+
+
+        public int CodeHeight
+        {
+            get { return (int)GetValue(CodeHeightProperty); }
+            set { SetValue(CodeHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CodeHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CodeHeightProperty =
+            DependencyProperty.Register("CodeHeight", typeof(int), typeof(CheckCode), new PropertyMetadata(60));
+
+
+        public CheckCode()
+        {
+            this.Loaded += CheckCode_Loaded;
+        }
+
+        private void CheckCode_Loaded(object sender, RoutedEventArgs e)
+        {
+            ImageSource = CreateCheckCodeImage(CreateCode(4), CodeWidth, CodeHeight);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _image = GetTemplateChild(ImageTemplateName) as System.Windows.Controls.Image;
+            if(_image!=null)
+                _image.PreviewMouseDown += _image_PreviewMouseDown;
+
+           
+        }
+       
+        private void _image_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ImageSource = CreateCheckCodeImage(CreateCode(4), CodeWidth, CodeHeight);
+        }
+
+        private static string CreateCode(int strLength)
+        {
+            var strCode = "abcdefhkmnprstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"; ;
+            var _charArray = strCode.ToCharArray();
+            var randomCode = "";
+            int temp = -1;
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < strLength; i++)
+            {
+                if (temp != -1)
+                {
+                    rand = new Random(i * temp * ((int)DateTime.Now.Ticks));
+                }
+                int t = rand.Next(strCode.Length - 1);
+                if (!string.IsNullOrWhiteSpace(randomCode))
+                {
+                    while (randomCode.ToLower().Contains(_charArray[t].ToString().ToLower()))
+                    {
+                        t = rand.Next(strCode.Length - 1);
+                    }
+                }
+                if (temp == t)
+                {
+                    return CreateCode(strLength);
+                }
+                temp = t;
+
+                randomCode += _charArray[t];
+            }
+            return randomCode;
+        }
+        private ImageSource CreateCheckCodeImage(string checkCode, int width, int height)
+        {
+            if (string.IsNullOrWhiteSpace(checkCode))
+                return null;
+            if (width <= 0 || height <= 0)
+                return null;
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+
+            using (DrawingContext dc = drawingVisual.RenderOpen())
+            {
+                dc.DrawRectangle(Brushes.White, new Pen(Brushes.Silver, 1D), new Rect(new Size(70, 23)));
+                //FormattedText formattedText = new FormattedText(
+                //    checkCode,
+                //    System.Globalization.CultureInfo.CurrentCulture,
+                //    FlowDirection.LeftToRight,
+                //    new Typeface(new FontFamily("Arial"), FontStyles.Oblique, FontWeights.Bold, FontStretches.Normal),
+                //    20.001D, new LinearGradientBrush(Colors.Green, Colors.DarkRed, 1.2D))
+                //{
+                //    MaxLineCount = 1,
+                //    TextAlignment = TextAlignment.Justify,
+                //    Trimming = TextTrimming.CharacterEllipsis
+                //};
+                var formattedText = new FormattedText(
+                      checkCode,
+                      CultureInfo.CurrentCulture,
+                      FlowDirection.LeftToRight,
+                      new Typeface(new FontFamily("Arial"), FontStyles.Oblique, FontWeights.Bold, FontStretches.Normal),
+                      20.001D,
+                      new LinearGradientBrush(Colors.Green, Colors.DarkRed, 1.2D),
+                      null,
+                      TextFormattingMode.Display,
+                      1d);
+                dc.DrawText(formattedText, new Point(3D, 0.1D));
+
+                for (int i = 0; i < 10; i++)
+                {
+                    int x1 = random.Next(width - 1);
+                    int y1 = random.Next(height - 1);
+                    int x2 = random.Next(width - 1);
+                    int y2 = random.Next(height - 1);
+
+                    dc.DrawGeometry(Brushes.Silver, new Pen(Brushes.Silver, 0.5D), new LineGeometry(new Point(x1, y1), new Point(x2, y2)));
+                }
+
+                for (int i = 0; i < 100; i++)
+                {
+                    int x = random.Next(width - 1);
+                    int y = random.Next(height - 1);
+                    SolidColorBrush c = new SolidColorBrush(Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255)));
+                    dc.DrawGeometry(c, new Pen(c, 1D), new LineGeometry(new Point(x - 0.5, y - 0.5), new Point(x + 0.5, y + 0.5)));
+                }
+
+                dc.Close();
+            }
+
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(70, 23, 96, 96, PixelFormats.Pbgra32);
+            renderBitmap.Render(drawingVisual);
+            return BitmapFrame.Create(renderBitmap);
+        }
+
+       
+    }
+}
