5570d2b773cc755e4249f269eb7deefa3dd2be65
Jia <654664168@qq.com>
2021-05-20 10:48:48 +0800

    optimize LiveXaml

--- src/Modules/Rubik.Module.Experiment/Models/ConstStrings.cs
+++ src/Modules/Rubik.Module.Experiment/Models/ConstStrings.cs
@@ -6,8 +6,7 @@ namespace Rubik.Module.Experiment.Models
 {
     public static class ConstStrings
     {
-        public const string LiveXamlTemplate = @"
-<Grid xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+        public const string LiveXamlTemplate = @"<Grid xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
       xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
       xmlns:controls=""clr-namespace:Rubik.Theme.Controls;assembly=Rubik.Theme""
       Background=""White"">
---
+++ src/Rubik.Theme.Extension/Utils/Converters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+using System.Globalization;
+
+using ICSharpCode.AvalonEdit.Editing;
+
+namespace Rubik.Theme.Extension.Utils
+{
+    public class LineNumbersWithoutLineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var leftMargins = (ObservableCollection<UIElement>)value;
+            return leftMargins.Where(lm => !DottedLineMargin.IsDottedLineMargin(lm));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
